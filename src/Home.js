5e6d9449469ffb5f507aeb862ee94a9b1d0be0ef
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-02-12 22:13:34 -0300

    changed pathing of home button to work on netlify

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 
 
 
-export default class Home extends Component {
+export default withRouter(class Home extends Component {
+    handleChange = (e) => {
+
+        this.props.history.push('/search')
+    }
+
     render() {
-
-        function windowChange() {
-            window.location.href = "/search";
-        }
-
         return (
             <div className="home-page">
                 <img className="pokeball" alt="pokeball" src="https://pngimg.com/uploads/pokeball/pokeball_PNG21.png" />
-                <button className="enter-button" onClick={windowChange}> ENTER
+                <button className="enter-button" onClick={this.handleChange}> ENTER
                 </button>
             </div>
         )
     }
-}
+})
