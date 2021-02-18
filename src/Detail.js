a46814703996e7cb924b380acf6536268fb8c195
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-02-17 21:51:27 -0300

    added spinner on detail page

--- src/Detail.js
+++ src/Detail.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import request from 'superagent';
+import Spinner from './Spinner.js';
 
 export default class Detail extends Component {
     state = {
-        pokeData: {}
+        pokeData: {},
     }
     componentDidMount = async () => {
+
+        this.setState({ loading: true });
 
         const data = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.props.match.params.specific}`);
 
         this.setState({
+            loading: false,
 
             pokeData: data.body.results.find(item => item.pokemon === this.props.match.params.specific),
 
@@ -25,16 +29,19 @@ export default class Detail extends Component {
 
         return (
             <div>
-
-                <div >
-                    <img alt="pokemon" src={this.state.pokeData.url_image}></img>
-                    <div >
-                        <div>name: {this.state.pokeData.pokemon} </div>
-                        <div>type: {this.state.pokeData.type_1}</div>
-                        <div>attack: {this.state.pokeData.attack} </div>
-                        <div>defense: {this.state.pokeData.defense} </div>
-                    </div>
-                </div>
+                {
+                    this.state.loading
+                        ? <Spinner />
+                        : <div >
+                            <img alt="pokemon" src={this.state.pokeData.url_image}></img>
+                            <div >
+                                <div>name: {this.state.pokeData.pokemon} </div>
+                                <div>type: {this.state.pokeData.type_1}</div>
+                                <div>attack: {this.state.pokeData.attack} </div>
+                                <div>defense: {this.state.pokeData.defense} </div>
+                            </div>
+                        </div>
+                }
             </div>
         )
     }
